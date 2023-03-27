6e24dac03d88311b0d1ea63d5dd018c75c215d70
Leandro <leandro.almeida@uvvnet.com.br>
2023-03-26 21:04:56 -0300

    estrutura projero de testes

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            // TODO;
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            // TODO;
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            // TODO;
+            Assert.Inconclusive();
+        }
+
+    }
+}
---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79a92158-af38-49c0-92c8-71ccef83fc96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
